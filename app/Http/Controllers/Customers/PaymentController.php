94757ddb8c230ab21a4e97b3ec55a2579115802d
Alfaben <zrav420@gmail.com>
2020-06-15 12:54:18 +0700

    update payment and .env

--- app/Http/Controllers/Customers/PaymentController.php
+++ app/Http/Controllers/Customers/PaymentController.php
@@ -32,7 +32,7 @@ class PaymentController extends Controller
     public function index($invoice)
     {
         $user = Auth::user();
-        $order = Order::where(['invoice' => strval($invoice), 'status' => 'pending', 'customer_id' => $user->id])->first();
+        $order = Order::where(['customer_id' => $user->id])->orderBy('id', 'desc')->first();
         if($order){
             if($order->status != 'pending'){
                 return redirect('/?message=payment_required');
@@ -272,6 +272,11 @@ class PaymentController extends Controller
                     'message' => 'Invoice berhasil di verifikasi'
                 ], 200);
             }
+        }else{
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'Invoice tidak dapat ditemukan',
+            ], 404);
         }
 
     }
