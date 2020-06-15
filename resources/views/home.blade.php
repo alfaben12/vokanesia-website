20a5bbacbffa08472d7691f3b4dc2d098b3b002e
Ismail Marzuki <44132026+imarzuki@users.noreply.github.com>
2020-06-15 14:09:45 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,11 @@
 
 <!-- banner start here -->
 @include('_partials.homes.banner')
-<!-- end of banner -->
 
 <!-- keunggulan start here -->
 @include('_partials.homes.keunggulan')
-<!-- end of keunggulan -->
 
 <!-- ebook reviews -->
 @include('_partials.homes.ebooks')
-<!-- end of ebook reviews -->
 
 @endsection
